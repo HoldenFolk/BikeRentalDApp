f1ac715983dfc851d814e0e35056debd49e44ff0
HoldenFolk <holdenfolk1@gmail.com>
2024-03-17 23:12:34 -0400

    Add basic setup for a smart contact and delpoy scripts

---
+++ m5/smartContract/settings.js
@@ -0,0 +1,6 @@
+const settings = {
+    API_KEY: process.env.API_KEY || "https://eth-sepolia.g.alchemy.com/v2/YCS-f96jtBAhln6Hht7I1mueFE8knf9z",
+    PRIVATE_WALLET_KEY: process.env.PRIVATE_WALLET_KEY || "49b560abb2c29b5175125c9859ab546e7ee4185165790960abe19b54426621a8",
+};
+
+export default settings;
