3c6f5bc148613d433b47300c3a9bcba97050e14f
Anto1Roy <antonin1roy@hotmail.com>
2024-03-22 14:13:59 -0400

    newe deployment fail

--- m5/frontend/src/util/interact.js
+++ m5/frontend/src/util/interact.js
@@ -1,74 +1,89 @@
 import { ethers } from "ethers";
-require('dotenv').config();
+require("dotenv").config();
 const contractABI = require("../contract-abi.json");
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(alchemyKey); 
+const web3 = createAlchemyWeb3(alchemyKey);
 const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
-
 
 let helloWorldContract;
 
 export const getHelloWorldContract = async () => {
-    const provider = new ethers.JsonRpcProvider(process.env.REACT_APP_SEPHOLIA_ENDPOINT);
+  const provider = new ethers.JsonRpcProvider(
+    process.env.REACT_APP_SEPHOLIA_ENDPOINT
+  );
 
-    console.log("Deploying contract to ", provider);
+  console.log("Deploying contract to ", provider);
 
-    const signer = new ethers.Wallet(process.env.REACT_APP_PRIVATE_WALLET_KEY, provider);
+  const signer = new ethers.Wallet(
+    process.env.REACT_APP_PRIVATE_WALLET_KEY,
+    provider
+  );
 
-    console.log("Deploying contract with the account: ", signer);
+  console.log("Deploying contract with the account: ", signer);
 
-    // replace with your contract's address and abi
-    helloWorldContract = new ethers.Contract(contractAddress, contractABI, signer);
-    // const connection = contract.connect(signer);
-    // console.log("Contract connected: ", connection);
-    console.log("Deploying contract...", helloWorldContract);
-    //return contract;
+  // replace with your contract's address and abi
+  helloWorldContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+  // const connection = contract.connect(signer);
+  // console.log("Contract connected: ", connection);
+  console.log("Deploying contract...", helloWorldContract);
+  //return contract;
 };
 
 // note to make it work -> have money in your wallet
 export const deployHelloWorldContract = async () => {
+  // Create an ethers provider
+  const provider = new ethers.JsonRpcProvider(
+    process.env.REACT_APP_SEPHOLIA_ENDPOINT
+  );
 
-    // Create an ethers provider
-    const provider = new ethers.JsonRpcProvider(process.env.REACT_APP_SEPHOLIA_ENDPOINT);
+  // Create a wallet with the private key
+  const signer = new ethers.Wallet(
+    process.env.REACT_APP_PRIVATE_WALLET_KEY,
+    provider
+  );
 
-    // Create a wallet with the private key
-    const signer = new ethers.Wallet(process.env.REACT_APP_PRIVATE_WALLET_KEY, provider);
+  const balance = await provider.getBalance(signer.address);
+  console.log("Balance: ", ethers.formatEther(balance), "ETH");
 
-    const balance = await provider.getBalance(signer.address);
-    console.log("Balance: ", ethers.formatEther(balance), "ETH");
+  // Get the contract factory
+  helloWorldContract = new ethers.ContractFactory(
+    contractABI,
+    process.env.REACT_APP_BYTE_CODE,
+    signer
+  );
 
-    // Get the contract factory
-    helloWorldContract = new ethers.ContractFactory(contractABI, process.env.REACT_APP_BYTE_CODE, signer);
+  console.log("Deploying contract: ", helloWorldContract);
+  const contract = await helloWorldContract.deploy("Hello World");
+  await contract.deployed();
 
-    console.log("Deploying contract: ", helloWorldContract);
-    const contract = await helloWorldContract.deploy("Hello World");
-    await contract.deployed();
+  console.log("message: ", contract.message());
 
-    console.log("message: ", contract.message());
+  // // Deploy the contract
+  // const transaction = await HelloWorldFactory.getDeployTransaction("test message", {
+  //     value: 0,
+  //     gasLimit: 3000000,
+  //     gasPrice: ethers.parseEther("0.1"),
+  // });
 
-    // // Deploy the contract
-    // const transaction = await HelloWorldFactory.getDeployTransaction("test message", {
-    //     value: 0,
-    //     gasLimit: 3000000,
-    //     gasPrice: ethers.parseEther("0.1"),
-    // });
+  // console.log("Deploy transaction: ", transaction);
 
+  // const gasEstimate = await signer.estimateGas(transaction);
 
-    // console.log("Deploy transaction: ", transaction);
+  // console.log("Gas estimate: ", gasEstimate);
 
-    // const gasEstimate = await signer.estimateGas(transaction);
+  // // Wait for the transaction to be mined
+  // const txReceipt = await signer.sendTransaction(transaction);
+  // console.log("Transaction receipt: ", txReceipt);
 
-    // console.log("Gas estimate: ", gasEstimate);
+  // console.log("Contract deployed instance", HelloWorldFactory);
 
-    // // Wait for the transaction to be mined
-    // const txReceipt = await signer.sendTransaction(transaction);
-    // console.log("Transaction receipt: ", txReceipt);
-
-    // console.log("Contract deployed instance", HelloWorldFactory);
-
-    return;
-}
+  return;
+};
 
 // export const helloWorldContract = new web3.eth.Contract(
 //     contractABI,
@@ -76,24 +91,17 @@ export const deployHelloWorldContract = async () => {
 // );
 
 export const loadCurrentMessage = async () => {
-    if (!helloWorldContract) {
-        await deployHelloWorldContract();
-    }
-    // log current contrract address
-    // console.log("Contract Address: ", helloWorldContract);
-    // const message = await helloWorldContract.message();
-    return "message";
+  if (!helloWorldContract) {
+    await deployHelloWorldContract();
+  }
+  // log current contrract address
+  // console.log("Contract Address: ", helloWorldContract);
+  // const message = await helloWorldContract.message();
+  return "message";
 };
 
+export const connectWallet = async () => {};
 
-export const connectWallet = async () => {
-  
-};
+export const getCurrentWalletConnected = async () => {};
 
-export const getCurrentWalletConnected = async () => {
-  
-};
-
-export const updateMessage = async (address, message) => {
-  
-};
+export const updateMessage = async (address, message) => {};
