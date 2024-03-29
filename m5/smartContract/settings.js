6ea371f36c0f002bd2a98427ec34036cac80f593
Pablo Collin <pablocollin@gmail.com>
2024-03-29 14:20:03 -0400

    Adding some tests

--- m5/smartContract/settings.js
+++ m5/smartContract/settings.js
@@ -1,5 +1,5 @@
 const settings = {
-    API_KEY: process.env.API_URL || "https://eth-sepolia.g.alchemy.com/v2/UdDAtAj_PksLGRtA2IkXRJbdJy0tgOr-",
+    API_KEY: process.env.API_URL || "https://eth-sepolia.g.alchemy.com/v2/I7s8Xku0KjuDxFhmGk93woitU3idmB5R",
     PRIVATE_WALLET_KEY: process.env.PRIVATE_WALLET_KEY || "88295edea46048352a51c25adc6b682e321c27806d57dc747ea957976ce00ed0",
 };
 
