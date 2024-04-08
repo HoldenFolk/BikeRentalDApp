0b417c8f9d4382d074bbd3b0982d6079ae0c8e0f
HoldenFolk <holdenfolk1@gmail.com>
2024-04-08 10:34:51 -0400

    remove keys and add env variables to aws

--- m5/smartContract/settings.js
+++ m5/smartContract/settings.js
@@ -1,6 +1,6 @@
 const settings = {
-    API_KEY: process.env.API_URL || "https://eth-sepolia.g.alchemy.com/v2/UdDAtAj_PksLGRtA2IkXRJbdJy0tgOr-",
-    PRIVATE_WALLET_KEY: process.env.PRIVATE_WALLET_KEY || "88295edea46048352a51c25adc6b682e321c27806d57dc747ea957976ce00ed0",
+    REACT_APP_API_KEY: process.env.API_URL || "",
+    REACT_APP_PRIVATE_WALLET_KEY: process.env.PRIVATE_WALLET_KEY || "",
 };
 
 module.exports = settings;
